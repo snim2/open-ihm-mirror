8cd6e24668baa9009a5dc02ac74c6e17ef2f0dee
snim2mirror <snim2mirror@users.noreply.github.com>
2014-03-22 15:55:01 +0000

    Updated to use new repo URLs

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 DEBUG = True
 
 REPO_PATH = './open-ihm'
-GITHUB_URL = 'git+ssh://git@github.com/snim2/open-ihm-mirror.git'
+GITHUB_URL = 'git+ssh://git@github.com/snim2mirror/openihm.git'
 GCODE_URL = 'https://code.google.com/p/open-ihm/'
